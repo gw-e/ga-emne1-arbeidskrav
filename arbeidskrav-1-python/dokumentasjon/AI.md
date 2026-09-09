6d98581b05094b909232453cd66d45b9aa96555a
Gabriel Waade-Eriksen <gabrielwaadeeriksen@gmail.com>
2026-09-09 16:08:59 +0200

    done som in task 2 and added documentasjon for ai usage

--- arbeidskrav-1-python/dokumentasjon/AI.md
+++ arbeidskrav-1-python/dokumentasjon/AI.md
@@ -6,6 +6,8 @@ Ingen bruk av AI her.
 
 # Oppgave 2:
 
+Her bruker jeg AI for å generere dummy data. Dette er fordi det tar mye kortere tid og energi, samt resultatet blir mere som virkelig data.
 ![alt text](image.png)
 
-Her bruker jeg AI for å generere dummy data. Dette er fordi det tar mye kortere tid og energi, samt resultatet blir mere som virkelig data.
+Her bruker jeg AI for å lage meg en mal da jeg ble usikker å hvor komplisert oppgaven skulle være.
+![alt text](image-1.png)
--- arbeidskrav-1-python/oppgave-2.py
+++ arbeidskrav-1-python/oppgave-2.py
@@ -33,8 +33,35 @@ STUDY_SESSIONS = [
 def clear_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+def error_message(message):
+    clear_terminal()
+    print(message)
+    input("\nPress enter to return: ")
+
+
 def main():
-    pass
+    clear_terminal()
+    print("Welcome to Task 2 :)")
+    print("\nWitch of these actions would you like to do:\n")
+    print("[1] View study sessions")
+    print("[2] Register study session")
+    print("[3] View total and average duration for completed sessions. ")
+    print("[4] Exit the program")
+
+    choice = input("\nPlease select an action by wrinting its number (1-7): ")
+
+    if choice == "1":
+        pass
+    elif choice == "7":
+        clear_terminal()
+        exit()
+    else:
+        error_message("Invalid input. Please enter the number of the action.")
+        return main()
+
+    input("\nPress enter to go back to menu: ")
+    return main()
+
 
 
 if __name__ == "__main__":
